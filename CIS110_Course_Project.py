bb6fffa3737499e1e70e8542d1edb41198c69dc0
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-10-26 09:45:01 -0700

    course project

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -1,1 +1,40 @@
-print("Hello World")
+print("Hello there!  I have an exciting tale to tell you about.")
+print("Before we begin. I have a few questions I need you to answer.")
+print("After typing your answer be sure to press the enter key.")
+input ("\nPress the enter key to continue...")
+
+seaTurtle = input("\nWhat would you like to name your sea turtle?  ")
+oceanName =input("\nCan you name an ocean?  ")
+islandName = input("\nWhat is an Island you want to vistit?  ")
+number = input("\nWhat are last two digits of your birth year?  ")
+favoriteBeach = input("\nWhat is the name of your favorite beach?  ")
+turtleType = input("\nCan you name one type of sea turtle?  ")
+
+print("\nLets Go!!!  ")
+print("\n")
+
+print("\nOnce upon a time there was a beautiful", turtleType, "named", seaTurtle + ".")
+print(seaTurtle, "loved to play on the beach.  ")
+print("\nOne day", seaTurtle, "was playing on the beach and the time passed him by quickly.  ")
+print(seaTurtle, "noticed he was no longer able to see his family.  ")
+print("\nSo", seaTurtle, "searched all around the island and the beach. His family was no where around.  ")
+print("He then decideds to go search the", oceanName, "ocean in hopes to find his family.  ")
+print("\nAfter searching the", oceanName, "ocean for", number, "days.  ")
+print(seaTurtle, "comes across", islandName, "Island.")
+print("\nHe wonders if his family could be at", favoriteBeach, "beach on", islandName, "Island.")
+print(seaTurtle, "decides to go to", islandName, "Island to search", favoriteBeach, "beach in hopes of finding his family.")
+print("\nOnce", seaTurtle, "arrives to", favoriteBeach, "and starts searching for his family.")
+print(seaTurtle, "comes across", turtleType, ",just like him, and he gets distracted by playing with them, \nand he looses track of time.")
+print("\nBefore he knew it", seaTurtle, "had spent", number, "days on", islandName, "Island he begins to miss his family.")
+print("So,", seaTurtle, "set back out to the", oceanName, "ocean to search for him family.")
+print("\n",seaTurtle, "has searched the", oceanName, "ocean for", number, "weeks and still no sign of him family.")
+print("Until one day while searching for his family", seaTurtle, " sees a family of", turtleType, "ahead and is curious if they could be his family.")
+print("\n",seaTurtle, "decides to check and see if that is his family.")
+print("As",seaTurtle, "got closer he soon realized the family of", turtleType, "is his family he has been searching for.")
+print("\n",seaTurtle, "is happy he has finally found his family, and is not alone anymore.")
+print("After traveling the", oceanName,"ocean", seaTurtle, "and his family decide to go to", islandName, "Island.")
+print("To make a home one", favoriteBeach, " beach, and live their remaining years happy together on", islandName, "Island.")
+
+
+
+
