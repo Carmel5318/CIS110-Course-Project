6aa254ae244da187fb62c48248f13de862708344
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-11-09 07:36:34 -0800

    update

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -25,7 +25,7 @@ print("\nHe wonders if his family could be at", favoriteBeach, "beach on", islan
 
 checkOutbeach = input("\nShould " + str(seaTurtle)+ " go check the beach for his fmaily?  Type yes or no:")
 if checkOutbeach.lower() == "yes" :
-    print("\n", seaTurtle, "decides to go to", islandName, "Island to search", favoriteBeach, "beach in hopes of finding his family.  ")
+    print("\n", seaTurtle, " decides to go to ", islandName, " Island to search ", favoriteBeach, " beach in hopes of finding his family.  ")
     print("\nOnce", seaTurtle, "arrives to", favoriteBeach, "beach and starts searching for his family.  ")
     print("\n", seaTurtle, "comes across", turtleType, ",just like him, and he gets distracted by playing with them, \nand he looses track of time.  ")
     print("\nBefore he knew it", seaTurtle, "had spent", number, "days on", islandName, "Island he begins to miss his family.  ")
@@ -33,20 +33,33 @@ if checkOutbeach.lower() == "yes" :
     print("\n",seaTurtle, "has searched the", oceanName, "ocean for", number, "weeks and still no sign of his family.  ")                               
     
 else:
-    print("\nAfter carefully thinking about it he decides not to seach", favoriteBeach, "on", islandName, "because he does not thing his family would be there. ")
-    print(seaTurtle, "continues to search the", oceanName, "in hope that he will soon find his family that he has lost. ")
+    print("\nAfter carefully thinking about it he decides not to seach ", favoriteBeach, " on ", islandName, " because he does not thing his family would be there. ")
+    print("\n", seaTurtle, " continues to search the ", oceanName, " in hope that he will soon find his family that he has lost. ")
 
-print("\nUntil one day while searching for his family", seaTurtle, " sees a family of", turtleType, "up ahead and is curious if they could be his family. ")
-seaTurtlefamily =input("\nShould" + str(seaTurle)+ "check to see if the family of" +str(turtleType)+ "is his family or not? Type yes or no:")
+print("\nUntil one day while searching for his family ", seaTurtle, " sees a family of ", turtleType, " up ahead and is curious if they could be his family. ")
+seaTurtlefamily =input("\nShould " + str(seaTurtle)+ " check to see if the family of " + str(turtleType)+ " is his family or not? Type yes or no:")
+
 if seaTurtlefamily.lower() == "yes" :
-    print("\n",seaTurtle, "decides to check and see if they are his family.  ")
-    print("\nAs",seaTurtle, "gets closer he soon realized that the family of", turtleType, "is his family he has been searching for.  ")
-    print("\n",seaTurtle, "is happy he has finally found his family, and is not alone anymore.  ")
+    print("\n",seaTurtle, " decides to check and see if they are his family.  ")
+    print("\nAs ",seaTurtle, " gets closer he soon realized that the family of " + str(turtleType)+ " is his family he has been searching for.  ")
+    print("\n",seaTurtle, " is happy he has finally found his family, and is not alone anymore.  ")
 else:
-    print("")
+    print("\n", seaTurtle, " decides to not check because they family of " + str (turtleType)+ " does not look like his family. ")
+    print("\n", seaTurtle, " continues his search for the family that he has lost.  ")
 
-print("\nAfter traveling the", oceanName,"ocean", seaTurtle, "and his family decide to go to", islandName, "Island.  ")
-print("\nTo make a home on", favoriteBeach, " beach, and live their remaining years happy together on", islandName, "Island.  ")
+
+if checkOutbeach.lower() == "yes" and seaTurtlefamily.lower() == "yes":
+    print("\nAfter traveling the " + str(oceanName)+ " ocean " + str(seaTurtle)+  " and his family decide to go to " + str(islandName)+ " Island.  ")
+    print("\nTo make a home on " + str(favoriteBeach)+ " beach, and live their remaining years happy together on " + str(islandName)+ " Island.  ")
+elif checkOutbeach.lower() == "yes":
+    print("\nAfter playing and makeing new friends at " + str(islandName)+ " and searching the " + str(oceanName)+ " for " + str(number)+ " years. ")
+    print("\nStill unable to find his family " + str(seaTurtle)+ " decides to go back to the " + str(islandName)+ " and be with his friend there on " + str(favoriteBeach)+ ".")
+    print("\nSo " + str(seaTurtle)+ " is not all alone."  )
+else:
+    print("\nAfter deciding to not search " + str(islandName)+ " and keep searching the " + str(oceanName)+ " for his family.  ")
+    print("\n", seaTurtle, " continues to search until he finds his lost family. He will search even if it takes " + str(number)+ " years.  ")
+    print("\n", seaTurtle, " is not going to give up on finding his family.  " )
+
 
 
 
